260acb84e324e902eaef484ae0ab1c9a612e5603
Rifi Ismail <ismail.elydi@gmail.com>
2020-07-19 17:25:36 +0200

    simple drag and drop working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import React , {useState}from 'react';
-import initialData from './initial-data'
-import Column from './components/Column'
+import React, { useState } from "react";
+import initialData from "./initial-data";
+import Column from "./components/Column";
+import { DragDropContext } from "react-beautiful-dnd";
 function App() {
-const [data,setData] = useState(initialData)
-console.log( );
-  return (
-    data.columnOrder.map(columnID => {
-      const column = data.columns[columnID]
-      const tasks = column.taskIds.map(
-        (taskId => {
-        return data.tasks[taskId]
-      })
-      )
-      return <Column key= {column.id} column={column} tasks= {tasks}/>
-    })
-  )
+  const [data, setData] = useState(initialData);
+  const onDragEnd = result => {
+    // TODO: REORDER OUR COLUMN
+    console.log(result);
+    const {destination,source,draggableId} = result
+    if(destination.droppableId === source.droppableId && destination.index === source.index){
+      // no change to record cause same column, same index
+      return;
+    }
+
+    const column = data.columns[source.droppableId]
+    const newTaskIds = Array.from(column.taskIds)
+
+    // delete the task in the Array
+    newTaskIds.splice(source.index,1)
+    // to remove it in this Index position
+    newTaskIds.splice(destination.index,0,draggableId)
+
+    const newColumn = {
+      ...column,
+      taskIds:newTaskIds
+    }
+
+    const newState = {
+      ...data,
+      columns: {
+        ...data.columns,
+        [newColumn.id] : newColumn,
+      }
+    }
+    setData(newState)
   }
 
+  return(
+  <DragDropContext
+  onDragEnd = {onDragEnd}
+  >
+    {
+      data.columnOrder.map((columnID) => {
+        const column = data.columns[columnID];
+        const tasks = column.taskIds.map((taskId) => {
+          return data.tasks[taskId];
+        });
+        return <Column key={column.id} column={column} tasks={tasks} />;
+      })
+    }
+  </DragDropContext>
+  )
+}
 
 export default App;
--- src/components/Column.js
+++ src/components/Column.js
@@ -1,28 +1,38 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Task from "./Task";
 
-import styled from 'styled-components'
-import Task from './Task'
+import { Droppable } from "react-beautiful-dnd";
 const Container = styled.div`
-margin:8px;
-border:1px solid lightgrey;
-border-radius : 2px;
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
 `;
 const Title = styled.h3`
-padding:8px;
+  padding: 8px;
 `;
 const TaskList = styled.div`
-padding:8px;
+  padding: 8px;
 `;
 
-export default function Column({column, tasks}) {
-    const listOfTasks = tasks.map(currentTask => {
-        return <Task key={currentTask.id} task={currentTask} />
-    })
-    return (
-        <Container>
-            <Title>{column.title}</Title>
-            <TaskList>{listOfTasks}</TaskList>
-
-        </Container>
-    )
+export default function Column({ column, tasks }) {
+  const listOfTasks = tasks.map((currentTask,index) => {
+    return <Task key={currentTask.id} task={currentTask} index={index}/>;
+  });
+  return (
+    <Container>
+      <Title>{column.title}</Title>
+      <Droppable droppableId={column.id}>
+        {(provided) => {
+            return <TaskList
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            >
+                {listOfTasks}
+                {provided.placeholder}
+            </TaskList>
+        }}
+      </Droppable>
+    </Container>
+  );
 }
